7ad34742a41b26c3e262e390a47dbd3c0d4bbf1b
Wai Hin Wong <henry@Wais-MacBook-Pro.local>
2020-04-21 17:00:56 +0800

    Updates

---
+++ precache-manifest.63d90d9934c63c79205fdd22ab8e1d24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b55e042df2768cad5cc9f2545d780482",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "103b442177b72655eaa1",
+    "url": "/monsters-rolodex/static/css/main.00ca2f20.chunk.css"
+  },
+  {
+    "revision": "a244513c933c345167e6",
+    "url": "/monsters-rolodex/static/js/2.cd21adb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.cd21adb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "103b442177b72655eaa1",
+    "url": "/monsters-rolodex/static/js/main.e03e6ac8.chunk.js"
+  },
+  {
+    "revision": "7488b7ba2cc40d446214",
+    "url": "/monsters-rolodex/static/js/runtime-main.373f4e5b.js"
+  }
+]);
